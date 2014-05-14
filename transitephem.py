e984adde21fe9b8fbdeabaf8cc829c5f2888b772
Brett Morris <brettmorris21@gmail.com>
2014-05-14 14:52:16 -0700

    tweaks and astropy integration

--- transitephem.py
+++ transitephem.py
@@ -9,12 +9,15 @@ import os
 import webbrowser
 from calculateEphemerides import *
 
+# Path to ".par" file, with the observatory parameters
 parfile = 'mro.par'
+
+# Path to the output HTML file -- this script will assume that the
+# "html_out" parameter in the .par file is set to True
 outputPath = os.path.join(os.path.dirname(os.path.abspath(__file__)),'outputs','eventReport.html')
 
+# Calculate the ephemeris for all visible planets within stated limits
 calculateEphemerides(parfile)
+
+# Open the HTML output in a new tab of the default browser
 webbrowser.open_new_tab("file:"+2*os.sep+outputPath)
-
-
-#if self.htmlBox.userParams["rbHtmlOut"].GetValue() == True:
-#                        webbrowser.open_new_tab("file:"+2*os.sep+outputPath)
